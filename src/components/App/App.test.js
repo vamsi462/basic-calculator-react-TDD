e1ca9e432e53b807fceeb5e31a30f321b19acaf6
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-12 22:44:43 +0530

    refactor the code to add new test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import {shallow} from 'enzyme'
-import App from './App'
+import React from "react";
+import {shallow} from "enzyme";
+import App from "./App";
 
-describe('App', () =>{
-  it('should render a <div/>',()=>{
-    const wrapper = shallow(<App/>)
-    expect(wrapper.find('div').length).toEqual(1);
+describe("App", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<App />)));
+
+  it("should render a <div/>", () => {
+    expect(wrapper.find("div").length).toEqual(1);
   });
+
+  
 });
