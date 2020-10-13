3b214717fb50b7af785db388efe7c60f302c0843
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 11:22:17 +0530

    write shallow render test

---
+++ src/components/Keypad/keypad.spec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {shallow} from 'enzyme';
+import Keypad from './Keypad'
+
+describe('Keypad',()=>{
+    let wrapper;
+
+    beforeEach(()=> {
+        wrapper =shallow(<Keypad/>)
+    })
+})
