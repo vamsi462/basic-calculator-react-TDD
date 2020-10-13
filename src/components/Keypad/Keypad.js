066bd971fdf9c788b92aa843a1197bdeb08bd1df
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 11:54:26 +0530

    updated the component with necessary proptypes to pass the test

---
+++ src/components/Keypad/Keypad.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Keypad({callOperator,numbers, operators,setOperator,updateDisplay}) {
+    return (
+        <div className="keypad-container">
+            
+        </div>
+    )
+}
+
+Keypad.propTypes = {
+    callOperator:PropTypes.func.isRequired,
+    numbers:PropTypes.array.isRequired,
+    operators:PropTypes.array.isRequired,
+    setOperator:PropTypes.func.isRequired,
+    updateDisplay:PropTypes.func.isRequired
+}
+
+export default Keypad
+
