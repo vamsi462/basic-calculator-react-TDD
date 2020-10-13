bb68ad84366a4d4317923aac7a54177dbdfcbaa2
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 13:09:40 +0530

    added styles

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Keypad.css'
 
 function Keypad({callOperator,numbers, operators,setOperator,updateDisplay}) {
+
 const numberKeys = numbers.map(number =><p key={number}>{number}</p>)
+const operatorKeys = operators.map(operator =><p key={operator}>{operator}</p>)
     return (
         <div className="keypad-container">
             <div className="numbers-container">
                 {numberKeys}
+            </div>
+            <div className = "operators-container">
+                 {operatorKeys}
             </div>
         </div>
     )
--- src/components/Keypad/keypad.spec.js
+++ src/components/Keypad/keypad.spec.js
@@ -15,13 +15,22 @@ describe('Keypad',()=>{
                         updateDisplay={jest.fn()}/>)
     })
 
-    it('should render 2 <div />\'s', () => {
-        expect(wrapper.find('div').length).toEqual(2);
+    it('should render 3 <div />\'s', () => {
+        expect(wrapper.find('div').length).toEqual(3);
     })
 
-    it('should render values of numbers',()=>{
-        wrapper.setProps({numbers:['0','1','2']});
-        expect(wrapper.find('.numbers-container').text()).toEqual('012');
-    })
+   it('renders the values of numbers', () => {
+       wrapper.setProps({
+           numbers: ['0','1','2']
+       });
+       expect(wrapper.find('.numbers-container').text()).toEqual('012');
+   });
+
+   it('renders the values of operators', () => {
+       wrapper.setProps({
+           operators: ["+","-","/","*"]
+       });
+       expect(wrapper.find('.operators-container').text()).toEqual('+-/*');
+   });
 
 })
