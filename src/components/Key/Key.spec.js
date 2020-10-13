a31b28042248fd4f6a9a7861eb608a678d0ffa1c
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 15:06:21 +0530

    added snapshot test

--- src/components/Key/Key.spec.js
+++ src/components/Key/Key.spec.js
@@ -8,11 +8,18 @@ describe('Key',()=>{
         wrapper = shallow(
             <Key
             keyAction={jest.fn()}
-            keytype={''}
-            keyvalue={''}/>
+            keyType={''}
+            keyValue={''}/>
         )
     })
+    it('should render correctly', () => expect(wrapper).toMatchSnapshot());
     it('should render a <div/>',()=>{
         expect(wrapper.find('div').length).toEqual(1)
+    })
+    it('should render  the value of keyvalue',()=>{
+        wrapper.setProps({
+            keyValue:'test'
+        })
+        expect(wrapper.text()).toEqual('test')
     })
 })  
