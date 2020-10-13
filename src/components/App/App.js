f50d83eefde1fe8963ec8ef977dd6d84cc1f50c3
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 10:26:15 +0530

    updated with children

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './App.css'
 import Calculator from '../Calculator/Calculator'
 
+
 const App = () => {
   return <div className="app-container">
            <Calculator/>
+         
          </div>;
 }
 
