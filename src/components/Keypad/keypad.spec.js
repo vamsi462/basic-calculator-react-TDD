539e4c657f59990e33b40c755134d8b20c17bda1
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 12:33:10 +0530

    test for numbers

--- src/components/Keypad/keypad.spec.js
+++ src/components/Keypad/keypad.spec.js
@@ -18,5 +18,10 @@ describe('Keypad',()=>{
     it('should render a <div/>',()=>{
         expect(wrapper.find('div').length).toEqual(1);
     })
+
+    it('should render values of numbers',()=>{
+        wrapper.setProps({numbers:['0','1','2']});
+        expect(wrapper.find('.numbers-container').text()).toEqual('012');
+    })
     
 })
