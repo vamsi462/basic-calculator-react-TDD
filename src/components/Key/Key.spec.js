cb529e31e8895bb05c54b8f15bf72af42ae83208
vamsi krishna <vamsikrishnaadusumalli999@gmail.com>
2020-10-13 14:38:21 +0530

    shallow render for the file existence

---
+++ src/components/Key/Key.spec.js
@@ -0,0 +1,18 @@
+import React  from 'react'
+import{shallow} from 'enzyme'
+import Key from  './Key'
+
+describe('Key',()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(
+            <Key
+            keyAction={jest.fn()}
+            keytype={''}
+            keyvalue={''}/>
+        )
+    })
+    it('should render a <div/>',()=>{
+        expect(wrapper.find('div').length).toEqual(1)
+    })
+})  
